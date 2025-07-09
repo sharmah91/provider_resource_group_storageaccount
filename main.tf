a1c05062bc1155b247b291bcfad9140146cc7432
Hitesh Sharma <sharmah91@gmail.com>
2025-07-09 22:34:00 +0530

    teeno chiz

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "6f89d107-0df7-4bf9-9a60-43a792310639"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "RH_Hitesh_1"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "stg" {
+  name                     = "behenchodstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}

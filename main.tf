f994dbceae04a2840ae6d93c84db03614bac06ae
Hitesh Sharma <sharmah91@gmail.com>
2025-07-09 23:44:29 +0530

    storage account hata diya hai

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ resource "azurerm_resource_group" "rg1" {
   location = "East US"
 }
 
-resource "azurerm_storage_account" "stg" {
-  name                     = ""
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "stg" {
+#   name                     = ""
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }

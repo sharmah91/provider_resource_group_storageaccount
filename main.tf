986cb2d1f7297c87a5b600079c7b5ffe751b9934
Hitesh Sharma <sharmah91@gmail.com>
2025-07-09 22:46:17 +0530

    storage account ka naam hata diya

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "rg1" {
 }
 
 resource "azurerm_storage_account" "stg" {
-  name                     = "behenchodstorage"
+  name                     = ""
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
